44ef1783807c081ac33aed9acfb35441524ba29e
Aditya01237 <pareekaditya01@gmail.com>
2026-03-20 14:57:56 +0530

    Starting SwasthyaSetu Project

---
+++ SwasthyaSetu/src/main/java/com/medicine/SwasthyaSetu/SwasthyaSetuApplication.java
@@ -0,0 +1,13 @@
+package com.medicine.SwasthyaSetu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwasthyaSetuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwasthyaSetuApplication.class, args);
+	}
+
+}
