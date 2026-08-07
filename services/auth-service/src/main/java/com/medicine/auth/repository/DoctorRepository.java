984925510a0d76ea692606d08bb908b3a86be46b
ADITYA PAREEK <80575917+Aditya01237@users.noreply.github.com>
2026-08-07 15:52:06 +0530

    Query doctor accounts by profile ID

--- services/auth-service/src/main/java/com/medicine/auth/repository/DoctorRepository.java
+++ services/auth-service/src/main/java/com/medicine/auth/repository/DoctorRepository.java
@@ -7,4 +7,5 @@ import java.util.Optional;
 
 public interface DoctorRepository extends JpaRepository<Doctor, Long> {
     Optional<Doctor> findByEmail(String email);
+    Optional<Doctor> findByProfileId(Long profileId);
 }
