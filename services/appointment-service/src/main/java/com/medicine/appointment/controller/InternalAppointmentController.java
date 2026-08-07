80bba17fba43a1b81effbfc727f2f74f0aedf6e2
ADITYA PAREEK <80575917+Aditya01237@users.noreply.github.com>
2026-08-07 14:49:23 +0530

    Require service token for appointment read model

--- services/appointment-service/src/main/java/com/medicine/appointment/controller/InternalAppointmentController.java
+++ services/appointment-service/src/main/java/com/medicine/appointment/controller/InternalAppointmentController.java
@@ -1,10 +1,12 @@
 package com.medicine.appointment.controller;
 
 import com.medicine.appointment.dto.AppointmentReadModelResponse;
+import com.medicine.appointment.security.InternalServiceAuthorizer;
 import com.medicine.appointment.service.AppointmentService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,13 +15,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class InternalAppointmentController {
 
     private final AppointmentService appointmentService;
+    private final InternalServiceAuthorizer internalServiceAuthorizer;
 
-    public InternalAppointmentController(AppointmentService appointmentService) {
+    public InternalAppointmentController(AppointmentService appointmentService,
+                                         InternalServiceAuthorizer internalServiceAuthorizer) {
         this.appointmentService = appointmentService;
+        this.internalServiceAuthorizer = internalServiceAuthorizer;
     }
 
     @GetMapping("/{id}/read-model")
-    public ResponseEntity<AppointmentReadModelResponse> readModelSnapshot(@PathVariable("id") Long id) {
+    public ResponseEntity<AppointmentReadModelResponse> readModelSnapshot(
+            @PathVariable("id") Long id,
+            @RequestHeader(value = InternalServiceAuthorizer.HEADER, required = false) String internalToken) {
+        internalServiceAuthorizer.requireValid(internalToken);
         return ResponseEntity.ok(appointmentService.getReadModelSnapshot(id));
     }
 }
