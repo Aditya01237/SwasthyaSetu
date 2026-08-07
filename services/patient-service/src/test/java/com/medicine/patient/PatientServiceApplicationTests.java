876377921d8391cc43489e89ab32f9947cde39c2
ADITYA PAREEK <80575917+Aditya01237@users.noreply.github.com>
2026-08-07 15:11:47 +0530

    Keep patient H2 tests isolated from Flyway

--- services/patient-service/src/test/java/com/medicine/patient/PatientServiceApplicationTests.java
+++ services/patient-service/src/test/java/com/medicine/patient/PatientServiceApplicationTests.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
         "spring.datasource.username=sa",
         "spring.datasource.password=",
         "spring.jpa.hibernate.ddl-auto=create-drop",
+        "spring.flyway.enabled=false",
         "ai.service.url=http://localhost:8000",
         "spring.rabbitmq.listener.simple.auto-startup=false",
         "management.health.rabbit.enabled=false"
