2adab10b29a6ae99cbd22465135e8198e50ff2c6
ADITYA PAREEK <80575917+Aditya01237@users.noreply.github.com>
2026-08-07 14:57:55 +0530

    Add hospital outbox repository

---
+++ services/hospital-service/src/main/java/com/medicine/hospital/outbox/OutboxEventRepository.java
@@ -0,0 +1,8 @@
+package com.medicine.hospital.outbox;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface OutboxEventRepository extends JpaRepository<OutboxEvent, Long> {
+    List<OutboxEvent> findTop100ByPublishedAtIsNullOrderByCreatedAtAsc();
+}
